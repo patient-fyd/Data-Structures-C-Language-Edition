bb942c3ffae58f0fe7ffbce16756539c29d1ea79
GanFengYu <patient.fyd@gmail.com>
2024-05-07 09:43:50 +0800

    修改测试代码

--- src/testDemo/headNodeSingleLinkedList_test.c
+++ src/testDemo/headNodeSingleLinkedList_test.c
@@ -1,12 +1,27 @@
 #include "../../include/list/headNodeSingleLinkedList.h"
 
-void hlnklist_test() {
-    struct link_node hl;
-    head_init(&hl);
-    head_insert(&hl, 1, 10);
-    head_insert(&hl, 2, 20);
-    head_insert(&hl, 3, 30);
-    head_insert(&hl, 4, 40);
-    head_insert(&hl, 5, 50);
-    head_display(&hl);
+void HeadNodeSingleLinkedList_test() {
+    Node *head = HeadInit();
+    if (head == NULL) {
+        printf("内存分配失败\n");
+        return;
+    }
+
+    HeadInsert(head, 0, 10);
+    HeadInsert(head, 1, 20);
+    HeadInsert(head, 2, 30);
+    HeadInsert(head, 3, 40);
+    HeadInsert(head, 4, 50);
+
+    printf("单链表元素: ");
+    HeadDisplay(head);
+
+    // 释放内存
+    Node *p = head->next;
+    while (p != NULL) {
+        Node *temp = p;
+        p = p->next;
+        free(temp);
+    }
+    free(head);
 }
